ed3b639fd4dfc7c491908f5c7c64ae1571d6cb45
Abin Joshua Hermon <abinjoshua3@gmail.com>
2026-05-19 17:33:39 +0530

    [UPDT]19/05/2026

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "POS Product Owner",
+    'version': "1.0",
+    'license': "LGPL-3",
+    'summary': """POS Product Owner, Designed to customize the POS module""",
+    'description': """POS Product Owner, Designed to customize the POS module, adds a product owner field in the pos orderline and in the receipts""",
+    'author': "Cybrosys",
+    'website': "http://www.cybrosys.com",
+    'sequence': 1,
+    'application': True,
+    'depends': ['base','point_of_sale','sale_management'],
+    'auto_install': True,
+    'data': ['views/product_product_views.xml',
+             ],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_product_owner/static/src/**/*',
+        ],
+    }
+}
